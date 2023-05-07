c9def8842597834a9cdbf53c166c3d212a7a6fb9
alepsiv01 <125842115+alepsiv01@users.noreply.github.com>
2023-05-07 16:40:12 -0600

    Create Main.java

---
+++ Act_E_3/Main.java
@@ -0,0 +1,12 @@
+package Act_Ex_3;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        //Ventana ventana = new Ventana("Layouts");
+        VentanaGrid ventanaGrid = new VentanaGrid("Ventana Layout Cuadricula");
+
+    }
+}
