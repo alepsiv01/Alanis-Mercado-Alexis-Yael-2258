7232ca419e7b397967a7999b765c595bfbef47d7
alepsiv01 <125842115+alepsiv01@users.noreply.github.com>
2023-05-13 22:29:24 -0600

    Main.java

---
+++ Tarea_12/Main.java
@@ -0,0 +1,8 @@
+package Tarea_12;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Ventana ventana = new Ventana("Ejemplo tabla MVC");
+    }
+}
